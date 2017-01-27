695f865dcd6c7dd0ad7fa29a2c059e92a3a60b70
masondomain <mason.hu@domain.com.au>
2017-01-27 17:16:01 +1100

    changes

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
+const bodyParser = require("body-parser");
 const config = require('./webpack.config.js');
 
+const entries = [];
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 3000 : process.env.PORT;
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 if (isDeveloping) {
   const compiler = webpack(config);
@@ -28,9 +32,27 @@ if (isDeveloping) {
 
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
-  app.get('*', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-    res.end();
+  app.get('/', function response(req, res) {
+    //res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+    //res.end();
+    response.render("index");
+  });
+
+  app.get("/new-entry", function(request, response) {
+    response.render("new-entry");
+  });
+
+  app.post("/new-entry", function(request, response) {
+    if (!request.body.title || !request.body.body) {
+      response.status(400).send("Entries must have a title and a body.");
+      return;
+    }
+    entries.push({
+      title: request.body.title,
+      content: request.body.body,
+      published: new Date()
+    });
+    response.redirect("/");
   });
 } else {
   app.use(express.static(__dirname + '/dist'));
@@ -39,7 +61,10 @@ if (isDeveloping) {
   });
 }
 
-app.listen(port, '0.0.0.0', function onStart(err) {
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.listen(port, '127.0.0.1', function onStart(err) {
   if (err) {
     console.log(err);
   }
